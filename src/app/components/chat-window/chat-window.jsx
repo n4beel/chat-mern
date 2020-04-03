3e34089d692fb6bd93ac2be16ebd7a45e43bb8a1
Nabeel <na268666@gmail.com>
2020-04-04 04:43:52 +0500

    border radius fixed

--- src/app/components/chat-window/chat-window.jsx
+++ src/app/components/chat-window/chat-window.jsx
@@ -6,31 +6,32 @@ const useStyles = makeStyles((theme) => ({
 		height: 460,
 		width: 360,
 		position: "absolute",
-		bottom: 36,
+		bottom: 20,
 		right: 112,
 		boxShadow: "1px 1px 6px #00000029",
-  },
-  chatContainer:{
-    backgroundColor: "white",
-    height: '100%',
-    borderRadius: 5,
-    position: 'relative',
-    top: 0,
-    left: 0,
-    zIndex: '10'
-  },
-  chatFin:{
-    height: 16,
-    width: 16,
-    backgroundColor: 'white',
-    transform: 'translateY(0px) rotate(45deg)',
-    borderRadius: 3,
-    position: 'absolute',
-    bottom: 19,
-    right: -8,
+		borderRadius: 16,
+	},
+	chatContainer: {
+		backgroundColor: "white",
+		height: "100%",
+		borderRadius: 16,
+		position: "relative",
+		top: 0,
+		left: 0,
+		zIndex: "10",
+	},
+	chatFin: {
+		height: 16,
+		width: 16,
+		backgroundColor: "white",
+		transform: "translateY(0px) rotate(45deg)",
+		borderRadius: 3,
+		position: "absolute",
+		bottom: 36,
+		right: -8,
 		boxShadow: "1px 1px 6px #00000029",
-    zIndex: '9'
-  }
+		zIndex: "9",
+	},
 }));
 
 const ChatWindow = () => {
